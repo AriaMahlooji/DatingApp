b2a329f710d7b1ca7aec44fe70d218e9c1f5187d
Aria Mahlooji <ariamahlooji.de@gmail.com>
2022-03-24 00:29:57 +0100

    End of section12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { max } from 'rxjs/operators';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,20 +13,25 @@ export class RegisterComponent implements OnInit {
 
   @Input() usersFromHomeComponent:any; //Parent to Child communication
   @Output() cancelRegister = new EventEmitter(); //Child to Parent communication
-
-  registerModel: any ={};
-  constructor(private accountService: AccountService) { }
+  maxDate : Date;
+  registerForm : FormGroup;
+  vaidationErrors: string[] =[];
+  constructor(private accountService: AccountService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeRegisterForm();
+    this.maxDate = new Date;
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   register()
   {
-    this.accountService.register(this.registerModel).subscribe(res=>{
-        console.log(res);
-        this.cancel();
+    // console.log(this.registerForm.value);
+    this.accountService.register(this.registerForm.value).subscribe(res=>{
+        this.router.navigateByUrl('/members');
       },
       err=>{
+        this.vaidationErrors = err;
         console.log(err);
       }
     );
@@ -32,4 +40,38 @@ export class RegisterComponent implements OnInit {
   {
     this.cancelRegister.emit(false);
   }
+
+  initializeRegisterForm()
+  {
+    this.registerForm = this.fb.group({
+      gender: ['female',Validators.required],
+      username: ['',Validators.required],
+      knownAs: ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required],
+      password: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(9), this.hasSpecialChar()]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe(()=>
+    {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    });
+  }
+
+  //validator function for matching passwords
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl)=> {
+      return control?.value === control?.parent?.controls[matchTo].value ? null : {isMatching: true}
+    }
+  }
+
+  hasSpecialChar(): ValidatorFn
+  {
+    return (control: AbstractControl)=>{
+      let regEx =new RegExp('.*(?=[^\\W]*\\W).*');
+      return regEx.test(control?.value) ? null : {includeSpecialChar: true}
+    }
+  }
+
 }
